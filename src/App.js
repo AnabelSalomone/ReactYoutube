f11ab0558ff85a23d6eee6f14b6e54a5f9127b51
AnabelSalomone <anabel.salomone@gmail.com>
2021-01-28 19:21:51 +0100

    wip: set search results

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Video from "./Video";
 import SearchResults from "./SearchResults";
 
 function App() {
-  const [searchKeyword, setSearchKeyword] = useState("");
+  const [searchKeyword, setSearchKeyword] = useState(""); // established from the Searchbar component
+  const [searchResult, setSearchResult] = useState({}); // established via the APIData function and sent to the SerachResults component
 
   const APIData = async (keyword) => {
     const url = "http://localhost:5000/Search/";
@@ -16,6 +17,7 @@ function App() {
     const json = await Promise.resolve(data.clone().json());
 
     console.log(json);
+    setSearchResult(Object.assign({}, json));
   };
 
   useEffect(() => {
@@ -31,7 +33,7 @@ function App() {
             <Video />
           </Route>
           <Route path="/SearchResults">
-            <SearchResults />
+            <SearchResults results={SearchResult} />
           </Route>
         </Switch>
       </Router>
