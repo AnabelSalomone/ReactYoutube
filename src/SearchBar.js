045f51e3d7071c688adfcb6eda09c3eb1ceb333d
AnabelSalomone <anabel.salomone@gmail.com>
2021-01-30 09:19:52 +0100

    handle submit and enter key on searchbar

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,26 +1,35 @@
 import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
-import {TextInput} from "react-materialize"
 
 const SearchBar = (props) => {
   let { handleKeyword } = props;
   let keyword = React.createRef();
   const [inputValue, setInputValue] = useState("");
 
+  const handleOnChange = () => {
+    setInputValue(keyword.current.value)
+  }
+
   const handleOnClick = () => {
     props.history.push("/SearchResults");
     handleKeyword(inputValue);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleOnClick();
+}
+  
+
   return (
     <div className="search-bar-container">
       <h2>My Youtube app</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           ref={keyword}
           placeholder="Search a video"
-          onChange={() => setInputValue(keyword.current.value)}
+          onChange={handleOnChange}
         />
         <button type="button" onClick={handleOnClick}>
           Search
